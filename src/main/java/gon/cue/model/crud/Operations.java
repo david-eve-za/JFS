7dce55e49b0f7c6732f9ab461f2cd6a8f481bc87
david-eve-za <david.eve.za@gmail.com>
2018-03-20 16:23:19 -0500

    Update Operations.java

--- src/main/java/gon/cue/model/crud/Operations.java
+++ src/main/java/gon/cue/model/crud/Operations.java
@@ -1,9 +1,6 @@
 package gon.cue.model.crud;
 
 import gon.cue.model.util.JOOQUtil;
-
-import java.io.File;
-
 import org.jooq.Record;
 import org.jooq.SelectWhereStep;
 import org.jooq.Table;
@@ -12,41 +9,28 @@ import org.jooq.impl.DSL;
 public class Operations extends JOOQUtil {
     private final static Operations _operations = new Operations();
 
-    static {
-        File f = new File("FileSystem.mv.db");
-
-        if (!f.exists()) {
-            f = null;
-            getOperations().createSchema();
-        }
-
-    }
-
     public static Operations getOperations() {
         return _operations;
     }
 
     public Operations() {
         super("FileSystem", "root", "toor");
+        super.createSchema();
     }
 
     public void close() {
         super.closeConnection();
     }
 
-    public Number Count(Table< ? > t) {
+    public Number Count(Table<?> t) {
         return create.select(DSL.count()).from(t).fetchOne(0, int.class);
     }
 
-    public Record create(Table< ? > t) {
+    public Record create(Table<?> t) {
         return create.newRecord(t);
     }
 
-    public SelectWhereStep< ? > findOn(Table< ? > t) {
+    public SelectWhereStep<?> findOn(Table<?> t) {
         return create.selectFrom(t);
-    }
-
-    public void reCreateDataBase() {
-        super.recreateSchema();
     }
 }
